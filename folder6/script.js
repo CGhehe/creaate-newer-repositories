cee4b14431a0f3ebbcf72291222587727242ad7e
Cg69-hash <cgskak@hotmail.com>
2023-03-21 15:41:27 +0100

    daf

--- folder6/script.js
+++ folder6/script.js
@@ -26,3 +26,47 @@ const chosenOne = harry;
 chosenOne.firstName = "(The chosen one)";
 
 console.log(harry);
+
+function showStudent(student) {
+  document.querySelector("#firstName").textContent = student.firstName;
+  document.querySelector("#middleName").textContent = student.middleName;
+  document.querySelector("#lastName").textContent = student.lastName;
+}
+
+harry.middleName = "James";
+ron.middleName = "Bilius";
+hermione.middleName = "Jean";
+
+const list = document.querySelector("#studentList");
+
+function addStudent(student) {
+  const listItem = document.createElement("li");
+  //const listText = document.createElement("span");
+
+  listItem.textContent = `${student.firstName}, ${student.middleName}, ${student.lastName}`;
+  list.appendChild(listItem);
+}
+
+addStudent(harry);
+addStudent(ron);
+addStudent(hermione);
+
+
+//southpark:
+
+name: text
+image: URL
+occupation: student,teacher
+age: Number
+voicedBy: text
+gender: male, female
+catchphrase: text 
+haircolor: text
+schoolgrade: Number
+episodes: xD 
+appereances: number 
+firstAppereance: hehe 
+
+
+
+

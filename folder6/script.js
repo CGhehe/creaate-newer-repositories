0bd00226218eb546cf262b9fd0db771c3d1b4317
Cg69-hash <cgskak@hotmail.com>
2023-03-21 13:17:27 +0100

    very cool

---
+++ folder6/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const harry = {
+  firstName: "Harry",
+  lastName: "Potter",
+};
+
+const ron = {
+  firstName: "Ron",
+  lastName: "Weasley",
+};
+
+const hermione = {
+  firstName: "Hermione",
+  lastName: "Granger",
+};
+
+console.log(harry);
+console.log(ron);
+console.log(hermione);

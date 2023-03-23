0ac4426131502c94d3293d968ccdbb1fc78c5166
Cg69-hash <cgskak@hotmail.com>
2023-03-23 15:11:50 +0100

    push

--- folder9/script.js
+++ folder9/script.js
@@ -3,8 +3,26 @@
 window.addEventListener("load", initApp);
 
 async function initApp() {
-    const ike = await getCharacter(app.json);
-    console.log(ike);
-    showCharacter(ike);
+  const ike = await getCharacter("app.json");
+  console.log(ike);
+  showCharacter(ike);
 }
 
+async function getCharacter(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+async function showCharacter(character) {
+  console.log(character);
+  document.querySelector("body").insertAdjacentHTML(
+    "beforebegin",
+    /*html*/ `
+            <article>
+            <h2>${character.name}</h2>
+            <img src="${character.image}" alt="">/
+            </article>
+        `
+  );
+}
